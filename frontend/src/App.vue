8d02b9d8e707261cd9a9956c379197d95c4387bc
Guilherme Soares <soarescbmsc@gmail.com>
2023-04-01 17:44:14 -0300

    WIP: Efetuando os testes e criando as funcções itens

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,17 +12,40 @@ import { reactive } from 'vue';
   });
 
   const addItem = function (product: any) {
-    order.items.push({ idProduct: product.idProduct, price: product.price, quantity: 1 });
+    const existingItem = order.items.find((item: any) => item.idProduct === product.idProduct);
+    if (!existingItem){
+      order.items.push({ idProduct: product.idProduct, price: product.price, quantity: 1 });
+    }else {
+      existingItem.quantity++;
+    }
+}
+
+  const decreaseItem = function (idProduct : any) {
+    const existingItem = order.items.find((item: any) => item.idProduct === idProduct);
+    if (!existingItem) return;
+      existingItem.quantity--;
   }
 
+  const increaseItem = function (idProduct : any) {
+    const existingItem = order.items.find((item: any) => item.idProduct === idProduct);
+    if (!existingItem) return;
+      existingItem.quantity++;
+  }
+  
   const getTotal = function () {
     let total = 0;
     for (const item of order.items){
-      total += item.price
+      total += item.price * item.quantity;
     }
     return total;
   }
+	const formatMoney = function (amount: number) {
+		return new Intl.NumberFormat("en-IN", { style: "currency", currency: "USD" }).format(amount);
+	}
 
+  const getProductById = function (idProduct: number) {
+		return products.find((product: any) => product.idProduct === idProduct);
+	}
 
 </script>
 
@@ -30,10 +53,17 @@ import { reactive } from 'vue';
   <div class="title">Checkout</div>
   <div v-for="product in products">
 		<span class="product-description">{{ product.description }}</span>
-		<span class="product-price">{{ product.price }}</span>
+		<span class="product-price">{{ formatMoney(product.price) }}</span>
     <button class="product-add-button" @click="addItem(product)">add</button>
 	</div>
-  <div class="total">{{ getTotal() }}</div>
+  <div class="total">{{ formatMoney(getTotal()) }}</div>
+  <div v-for="item in order.items">
+		<span class="item-description">{{ getProductById(item.idProduct)?.description }}</span>
+    <span class="item-quantity">{{ item.quantity }}</span>
+    <button class="item-increase-button" @click="increaseItem(item.idProduct)">+</button>
+    <button class="item-decrease-button" @click="decreaseItem(item.idProduct)">-</button>
+
+  </div>
 </template>
 
 <style scoped>
--- frontend/test/App.test.ts
+++ frontend/test/App.test.ts
@@ -1,23 +1,66 @@
 import { mount } from "@vue/test-utils";
 import AppVue from "../src/App.vue";
 
-test("Deve testar o Frontend", async function () {
-    const wrapper = mount(AppVue, {
-
-    });
-
-    expect(wrapper.get(".title").text()).toBe("Checkout");
-	expect(wrapper.findAll(".product-description").at(0)?.text()).toBe("A");
-	expect(wrapper.findAll(".product-price").at(0)?.text()).toBe("1000");
+test("Deve apresentar um pedido vazio", async function () {
+    const wrapper = mount(AppVue, {});
+    expect(wrapper.get(".title").text()).toBe("Checkout");   
+    expect(wrapper.findAll(".product-description").at(0)?.text()).toBe("A");
+	expect(wrapper.findAll(".product-price").at(0)?.text()).toBe("$1,000.00");
 	expect(wrapper.findAll(".product-description").at(1)?.text()).toBe("B");
-	expect(wrapper.findAll(".product-price").at(1)?.text()).toBe("5000");
+	expect(wrapper.findAll(".product-price").at(1)?.text()).toBe("$5,000.00");
 	expect(wrapper.findAll(".product-description").at(2)?.text()).toBe("C");
-	expect(wrapper.findAll(".product-price").at(2)?.text()).toBe("30");
+	expect(wrapper.findAll(".product-price").at(2)?.text()).toBe("$30.00");
+    expect(wrapper.get(".total").text()).toBe("$0.00");
+});
+
+test("Deve ter um pedido com apenas 1 item", async function () {
+    const wrapper = mount(AppVue, {});
+	expect(wrapper.findAll(".product-description").at(0)?.text()).toBe("A");
+	expect(wrapper.findAll(".product-price").at(0)?.text()).toBe("$1,000.00");
+    await wrapper.findAll(".product-add-button").at(0)?.trigger("click");
+    expect(wrapper.findAll(".item-description").at(0)?.text()).toBe("A");
+	expect(wrapper.findAll(".item-quantity").at(0)?.text()).toBe("1");   
+    expect(wrapper.get(".total").text()).toBe("$1,000.00");
+});
+
+test("Deve pedir varios items e quantidade maior que 1", async function () {
+    const wrapper = mount(AppVue, {});
     await wrapper.findAll(".product-add-button").at(0)?.trigger("click");
     await wrapper.findAll(".product-add-button").at(1)?.trigger("click");
     await wrapper.findAll(".product-add-button").at(2)?.trigger("click");
     await wrapper.findAll(".product-add-button").at(2)?.trigger("click");
     await wrapper.findAll(".product-add-button").at(2)?.trigger("click");
-    
-    expect(wrapper.get(".total").text()).toBe("6090");
+    expect(wrapper.findAll(".item-description").at(0)?.text()).toBe("A");
+	expect(wrapper.findAll(".item-quantity").at(0)?.text()).toBe("1");
+	expect(wrapper.findAll(".item-description").at(1)?.text()).toBe("B");
+	expect(wrapper.findAll(".item-quantity").at(1)?.text()).toBe("1");
+	expect(wrapper.findAll(".item-description").at(2)?.text()).toBe("C");
+	expect(wrapper.findAll(".item-quantity").at(2)?.text()).toBe("3");
+    expect(wrapper.get(".total").text()).toBe("$6,090.00");
+});
+
+test("Deve pedir varios itens e decrementar a quantidade do itens do pedido", async function () {
+    const wrapper = mount(AppVue, {});
+    await wrapper.findAll(".product-add-button").at(0)?.trigger("click");
+    await wrapper.findAll(".product-add-button").at(1)?.trigger("click");
+    await wrapper.findAll(".product-add-button").at(2)?.trigger("click");
+    await wrapper.findAll(".product-add-button").at(2)?.trigger("click");
+    await wrapper.findAll(".product-add-button").at(2)?.trigger("click");   
+    await wrapper.findAll(".item-decrease-button").at(2)?.trigger("click");
+    await wrapper.findAll(".item-decrease-button").at(2)?.trigger("click"); 
+    expect(wrapper.get(".total").text()).toBe("$6,030.00");
+    expect(wrapper.findAll(".item-quantity").at(2)?.text()).toBe("1");
+
+});
+
+test("Deve pedir varios itens e incrementar a quantidade do itens do pedido", async function () {
+    const wrapper = mount(AppVue, {});
+    await wrapper.findAll(".product-add-button").at(0)?.trigger("click");
+    expect(wrapper.get(".total").text()).toBe("$1,000.00");
+    expect(wrapper.findAll(".item-quantity").at(0)?.text()).toBe("1");
+    await wrapper.findAll(".item-increase-button").at(0)?.trigger("click");
+    await wrapper.findAll(".item-increase-button").at(0)?.trigger("click");
+    expect(wrapper.get(".total").text()).toBe("$3,000.00");
+    expect(wrapper.findAll(".item-quantity").at(0)?.text()).toBe("3");
+
 });
